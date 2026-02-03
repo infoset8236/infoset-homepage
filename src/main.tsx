c515e9f66152705f3d8d8ccb04f3a282d2ba0da0
happygxxg <kangyoonkyung@infoset.co.kr>
2026-02-03 17:48:49 +0900

    feat: set up fullpage.js for Home page

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import '@/styles/global.css';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import React from 'react';
+import '@/styles/global.css';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
 
